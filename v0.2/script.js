b2f633e248dc101ef5bba997f9646474e97b4b08
elbaron23 <imad.touzouz1@gmail.com>
2025-09-19 19:32:35 +0100

    *

---
+++ v0.2/script.js
@@ -0,0 +1,32 @@
+let htmlbtn = document.getElementById('html');
+let cssbtn = document.getElementById('css');
+let resultbtn = document.getElementById('result');
+
+let htmlCode = document.getElementById("htmlCode");
+let cssCode = document.getElementById("cssCode");
+let resultcode = document.getElementById("resultcode");
+
+
+htmlbtn.addEventListener('click', () => {
+    htmlCode.style.display = "block";
+    cssCode.style.display = "none";
+    resultcode.style.display = "none";
+});
+cssbtn.addEventListener('click', () => {
+    htmlCode.style.display = "none";
+    cssCode.style.display = "block";
+    resultcode.style.display = "none";
+});
+resultbtn.addEventListener('click', () => {
+    htmlCode.style.display = "none";
+    cssCode.style.display = "none";
+    resultcode.style.display = "block";
+    updateResult();
+});
+
+function updateResult() {
+    let html = htmlCode.value
+    let css = "<style>" + cssCode.value + "</style>"
+    resultcode.srcdoc = html + css
+    
+}
